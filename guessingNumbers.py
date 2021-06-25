544f15d514af19ef1c59be7a4040e6f561d7c5be
jagrit2008 <70403360+jagrit2008@users.noreply.github.com>
2021-06-25 22:45:20 +0530

    Add files via upload

---
+++ guessingNumbers.py
@@ -0,0 +1,8 @@
+import random = random.random(0,9) 
+print(random)
+    while chances < 5:
+    if guess == number:
+    print("CONGRATULATIONS YOU WON")
+    break
+    if not chances <5:
+        print("You lose !!! the number is",number)
